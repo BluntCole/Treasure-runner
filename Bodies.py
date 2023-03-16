f038281ed6633715ab108224143917f9bf238481
ColeBlunt <65433288+ColeBlunt@users.noreply.github.com>
2023-03-16 11:21:17 -0400

    adding ball

--- Bodies.py
+++ Bodies.py
@@ -1,8 +1,45 @@
 import pygame as pg
 from Box2D import *
+import Box2D as b2
 import math
 
 b2w = 100
+
+class Ball:
+    def __init__(self, world, x, y, radius, density=1, friction=0.3, restitution=0.5):
+        # Set up the physical properties of the ball
+        self.radius = radius
+        self.density = density
+        self.friction = friction
+        self.restitution = restitution
+
+        # Create the ball body
+        self.body = world.CreateDynamicBody(position=(x, y))
+        self.shape = b2.b2CircleShape(radius=radius)
+        self.fixture = self.body.CreateFixture(shape=self.shape, density=density,
+                                               friction=friction, restitution=restitution)
+
+        self.rect = pg.Rect(x - radius, y - radius, 2*radius, 2*radius)
+
+        self.on_ground = False
+
+        self.speed = -1000
+
+    def draw(self, screen, screen_height):
+        # Draw the ball on the screen
+        ball_position = self.body.position
+        ball_screen_pos = (int(ball_position[0]), int(screen_height - ball_position[1]))
+        pg.draw.circle(screen, (50, 50, 50), ball_screen_pos, self.radius)
+
+    def update(self):
+        if self.on_ground == False:
+            self.body.ApplyLinearImpulse(b2Vec2(-self.speed, -10000), self.body.worldCenter, True)
+            print(self.on_ground, "here 1")
+        if self.on_ground == True:
+            self.body.ApplyLinearImpulse(b2Vec2(-self.speed, 0), self.body.worldCenter, True)
+            self.on_ground = False
+            print(self.on_ground, "here 2")
+
 
 class Player(pg.sprite.Sprite):
     def __init__(self, x, y, frame_width, frame_height, world):
@@ -86,6 +123,7 @@ class Player(pg.sprite.Sprite):
     def move_right(self):
         self.is_moving = True
         self.direction = "right"
+
 
 class Tile(pg.sprite.Sprite):
     def __init__(self, pos, surf, groups):
--- GameEngine.py
+++ GameEngine.py
@@ -5,6 +5,8 @@ import pytmx
 import os
 from Bodies import Player
 from Bodies import Tile
+from Bodies import Ball
+
 
 class Camera:
     def __init__(self, width, height):
@@ -18,6 +20,7 @@ class Camera:
         y = -target.rect.y + int(screen.get_height() / 2)
         self.rect = pg.Rect(x, y, self.rect.width, self.rect.height)
 
+
 pg.init()
 
 mixer.init()
@@ -30,18 +33,22 @@ script_dir = os.path.dirname(os.path.abspath(__file__))
 map_path = os.path.join(script_dir, "GameMap", "GameMap.tmx")
 tiled_map = pytmx.util_pygame.load_pygame(map_path)
 
-wgravity = (0,100)
-world = b2World(wgravity, doSleep=True)
-print(wgravity)
-player = Player(50,150,32,64, world)
+world = b2World((0, 100), doSleep=True)
 
+player = Player(50, 150, 32, 64, world)
 gamer = pg.sprite.Group()
 gamer.add(player)
+
+ball = Ball(world, 150, 700, 20)
+# baller = pg.sprite.Group()
+# baller.add(ball)
 
 tile_group = pg.sprite.Group()
 non_pys_group = pg.sprite.Group()
 win_group = pg.sprite.Group()
 lose_group = pg.sprite.Group()
+start_group = pg.sprite.Group()
+phy_for_ball = pg.sprite.Group()
 
 # cam_group = pg.sprite.Group()
 # cam_group.add(tile_group, non_pys_group, win_group, lose_group)
@@ -52,37 +59,46 @@ mixer.music.load('music.mp3')
 # Play the music on loop
 mixer.music.play(-1)
 
-
 for layer in tiled_map.layers:
-    # if layer.name in ('player'):
     if layer.name in ('Physical for player and ball', 'physical for player'):
         for x, y, surf in layer.tiles():
             pos = (x * tiled_map.tilewidth, y * tiled_map.tileheight)
             Tile(pos=pos, surf=surf, groups=tile_group)
 
 for layer in tiled_map.layers:
-    # if layer.name in ('player'):
     if layer.name in ('stairs', 'decorate', 'wall'):
         for x, y, surf in layer.tiles():
             pos = (x * tiled_map.tilewidth, y * tiled_map.tileheight)
             Tile(pos=pos, surf=surf, groups=non_pys_group)
 
 for layer in tiled_map.layers:
-    # if layer.name in ('player'):
     if layer.name in ('lose'):
         for x, y, surf in layer.tiles():
             pos = (x * tiled_map.tilewidth, y * tiled_map.tileheight)
             Tile(pos=pos, surf=surf, groups=lose_group)
 
 for layer in tiled_map.layers:
-    # if layer.name in ('player'):
     if layer.name in ('win'):
         for x, y, surf in layer.tiles():
             pos = (x * tiled_map.tilewidth, y * tiled_map.tileheight)
             Tile(pos=pos, surf=surf, groups=win_group)
 
+for layer in tiled_map.layers:
+    if layer.name in ('start'):
+        for x, y, surf in layer.tiles():
+            pos = (x * tiled_map.tilewidth, y * tiled_map.tileheight)
+            Tile(pos=pos, surf=surf, groups=start_group)
+
+for layer in tiled_map.layers:
+    if layer.name in ('Physical for player and ball'):
+        for x, y, surf in layer.tiles():
+            pos = (x * tiled_map.tilewidth, y * tiled_map.tileheight)
+            Tile(pos=pos, surf=surf, groups=phy_for_ball)
+
 clock = pg.time.Clock()
 running = True
+
+pickup = False
 
 while running:
     for event in pg.event.get():
@@ -95,6 +111,9 @@ while running:
                 player.move_right()
             elif event.key == pg.K_SPACE:
                 player.jump()
+        elif event.type == pg.KEYUP:
+            player.is_moving = False
+            player.direction = None
 
     camera_x = player.rect.x - screen.get_width() / 2
     camera_y = player.rect.y - screen.get_height() / 2
@@ -125,8 +144,26 @@ while running:
                 print("you lose")
                 pg.quit()
 
+    for tile in start_group:
+        if tile.image != 0:
+            if player.rect.colliderect(tile.rect):
+                pickup = True
+                print("better run")
+
+    for tile in phy_for_ball:
+        if tile.image != 0:
+            if ball.rect.colliderect(tile.rect):
+                ball.on_ground = True
+                print(ball.on_ground)
+                break
+    else:
+        ball.on_ground = False
+        print(ball.on_ground)
+
     dt = clock.tick(60) / 1000.0
     world.Step(dt, 6, 2)
+
+    ball.update()
 
     # screen.fill((0, 0, 0))
     gamer.update(dt)
@@ -145,14 +182,17 @@ while running:
     for tile in lose_group:
         if tile.image != 0:
             screen.blit(tile.image, (tile.rect.x - camera_x, tile.rect.y - camera_y))
+    if pickup == False:
+        for tile in start_group:
+            if tile.image != 0:
+                screen.blit(tile.image, (tile.rect.x - camera_x, tile.rect.y - camera_y))
 
     screen.blit(player.image, (player.rect.x - camera_x, player.rect.y - camera_y))
 
-
+    ball.draw(screen, screen.get_height())
     pg.display.update()
     pg.display.flip()
 
 mixer.music.stop()
 
 pg.quit()
-
